78532cd5eab16febe6c76ae86ac0ab80d98a4a24
ASH0DANNY <ash22714497@gmail.com>
2024-10-30 18:21:33 +0530

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,24 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import MenuIcon from "@mui/icons-material/Menu";
 import CloseIcon from "@mui/icons-material/Close";
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
+  const menuBlock = useRef(null);
 
   const handleMenuClick = () => {
     open ? setOpen(false) : setOpen(true);
     console.log("Menu Clicked");
+    menuBlock.current.classList.remove("w-14", "h-14", "rounded-full");
+    menuBlock.current.classList.add("w-[3000px]", "h-[450px]", "bg-green-300");
   };
 
   return (
     <>
-      <div className="fixed top-5 right-5 w-14 h-14 shadow-lg z-40 rounded-full">
+      <div
+        ref={menuBlock}
+        className="fixed top-5 right-5 w-14 h-14 shadow-lg z-40 rounded-full"
+      >
         {open ? (
           <button
             className="w-full h-full cursor-pointer z-30"
@@ -31,7 +37,7 @@ const Navbar = () => {
 
         {/* Menu Open Block */}
         {open ? (
-          <div className="relative -top-8 right-[200px] w-[280px] h-[350px] bg-white"></div>
+          <div className="relative right-[200px] w-[280px] h-[350px] bg-white"></div>
         ) : null}
       </div>
     </>
